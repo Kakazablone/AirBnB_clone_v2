9ec83f2c808ee0ac7a835cecc746e35dd75a4275
Stephenmuhindi <bteneson42@gmail.com>
2024-01-15 14:35:30 -0800

    kutoa import

--- models/user.py
+++ models/user.py
@@ -5,7 +5,6 @@ from os import getenv
 import models
 import sqlalchemy
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
