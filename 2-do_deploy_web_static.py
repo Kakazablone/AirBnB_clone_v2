b6ec092b474b902413d3bced4d9fff8c5b71b611
Zablon Samba <zablonsamba@gmail.com>
2024-02-05 00:30:05 +0300

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,32 +11,32 @@ env.key_filename = '~/.ssh/school'
 
 def do_deploy(archive_path):
     """Deploy web files to server"""
-    if not exists(archive_path):
-        return False
+    if exists(archive_path):
+        # Upload archive
+        put(archive_path, '/tmp/')
 
-    # Upload archive
-    put(archive_path, '/tmp/')
+        timestamp = archive_path.split('.')[0][-14:]
+        sudo('mkdir -p \
+                /data/web_static/releases/web_static_{}/'
+             .format(timestamp))
 
-    timestamp = archive_path.split('.')[0][-14:]
-    sudo('mkdir -p \
+        # Uncompress archive, delete archive, Move files into Host
+        # web_static then remove the src web_static dir
+        sudo('tar -vxzf /tmp/web_static_{}.tgz -C \
             /data/web_static/releases/web_static_{}/'
-         .format(timestamp))
+             .format(timestamp, timestamp))
+        sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
+        sudo('mv /data/web_static/releases/web_static_{}/web_static/* \
+            /data/web_static/releases/web_static_{}/'
+             .format(timestamp, timestamp))
+        sudo('rm -rf \
+            /data/web_static/releases/web_static_{}/web_static'
+             .format(timestamp))
 
-    # Uncompress archive, delete archive, Move files into Host
-    # web_static then remove the src web_static dir
-    sudo('tar -vxzf /tmp/web_static_{}.tgz -C \
-        /data/web_static/releases/web_static_{}/'
-         .format(timestamp, timestamp))
-    sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
-    sudo('mv /data/web_static/releases/web_static_{}/web_static/* \
-        /data/web_static/releases/web_static_{}/'
-         .format(timestamp, timestamp))
-    sudo('rm -rf \
-        /data/web_static/releases/web_static_{}/web_static'
-         .format(timestamp))
-
-    # Delete pre-existing sym link and re-establish
-    sudo('rm -rf /data/web_static/current')
-    sudo('ln -s /data/web_static/releases/web_static_{}/ \
-        /data/web_static/current'.format(timestamp))
-    return True
+        # Delete pre-existing sym link and re-establish
+        sudo('rm -rf /data/web_static/current')
+        sudo('ln -s /data/web_static/releases/web_static_{}/ \
+            /data/web_static/current'.format(timestamp))
+        return True
+    else:
+        return False
