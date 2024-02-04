670c0027fe6359aacda93bd61aa6eacac54dd09d
Zablon Samba <zablonsamba@gmail.com>
2024-02-05 00:24:09 +0300

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -39,3 +39,4 @@ def do_deploy(archive_path):
     sudo('rm -rf /data/web_static/current')
     sudo('ln -s /data/web_static/releases/web_static_{}/ \
         /data/web_static/current'.format(timestamp))
+    return True
