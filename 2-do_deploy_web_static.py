5cb0cda4b6c6bfbca04abc0caba263fbaa29674a
Zablon Samba <zablonsamba@gmail.com>
2024-02-05 00:21:35 +0300

    Write a Fabric script (based on the file 1-pack_web_static.py) that distributes an archive to your web servers, using the function do_deploy

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -20,20 +20,20 @@ def do_deploy(archive_path):
     timestamp = archive_path.split('.')[0][-14:]
     sudo('mkdir -p \
             /data/web_static/releases/web_static_{}/'
-            .format(timestamp))
+         .format(timestamp))
 
     # Uncompress archive, delete archive, Move files into Host
     # web_static then remove the src web_static dir
     sudo('tar -vxzf /tmp/web_static_{}.tgz -C \
         /data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
+         .format(timestamp, timestamp))
     sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
     sudo('mv /data/web_static/releases/web_static_{}/web_static/* \
         /data/web_static/releases/web_static_{}/'
-            .format(timestamp, timestamp))
+         .format(timestamp, timestamp))
     sudo('rm -rf \
         /data/web_static/releases/web_static_{}/web_static'
-            .format(timestamp))
+         .format(timestamp))
 
     # Delete pre-existing sym link and re-establish
     sudo('rm -rf /data/web_static/current')
