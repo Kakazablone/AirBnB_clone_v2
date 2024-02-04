d8c2aa1966069e6ff7df12928517f9135281bbca
Zablon Samba <zablonsamba@gmail.com>
2024-02-04 10:41:43 +0300

    Distribute an archive to your web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """Fabric script (based on the file 1-pack_web_static.py)
 that distributes an archive to your web servers"""
-from fabric import Group, env
+from fabric.api import *
 from os.path import exists
 
 env.hosts = ["100.25.102.191", "100.26.161.26"]
@@ -13,30 +13,26 @@ def do_deploy(archive_path):
     """Deploy web files to server"""
     if not exists(archive_path):
         return False
-    with Group(*env.hosts,
-                     user=env.user,
-                     connect_kwargs={"key_filename": env.key_filename})as conn:
-        # Upload archive
-        conn.put(archive_path, '/tmp/')
 
-        timestamp = archive_path.split('.')[0][-14:]
-        conn.sudo('mkdir -p /data/web_static/releases/web_static_{}/'
-                  .format(timestamp))
+    # Upload archive
+    put(archive_path, '/tmp/')
 
-        # Uncompress archive, delete archive, Move files into Host
-        # web_static then remove the src web_static dir
-        conn.sudo('tar -vxzf /tmp/web_static_{}.tgz -C \
-                  /data/web_static/releases/web_static_{}/'
-                  .format(timestamp, timestamp))
-        conn.sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
-        conn.sudo('mv /data/web_static/releases/web_static_{}/web_static/* \
-                  /data/web_static/releases/web_static_{}/'
-                  .format(timestamp, timestamp))
-        conn.sudo('rm -rf \
-                  /data/web_static/releases/web_static_{}/web_static'
-                  .format(timestamp))
+    timestamp = archive_path.split('.')[0][-14:]
+    sudo('mkdir -p /data/web_static/releases/web_static_{}/'.format(timestamp))
 
-        # Delete pre-existing sym link and re-establish
-        conn.sudo('rm -rf /data/web_static/current')
-        conn.sudo('ln -s /data/web_static/releases/web_static_{}/ \
-                  /data/web_static/current'.format(timestamp))
+    # Uncompress archive, delete archive, Move files into Host
+    # web_static then remove the src web_static dir
+    sudo('tar -vxzf /tmp/web_static_{}.tgz -C \
+         /data/web_static/releases/web_static_{}/'
+         .format(timestamp, timestamp))
+    sudo('rm /tmp/web_static_{}.tgz'.format(timestamp))
+    sudo('mv /data/web_static/releases/web_static_{}/web_static/* \
+         /data/web_static/releases/web_static_{}/'
+         .format(timestamp, timestamp))
+    sudo('rm -rf \
+         /data/web_static/releases/web_static_{}/web_static'.format(timestamp))
+
+    # Delete pre-existing sym link and re-establish
+    sudo('rm -rf /data/web_static/current')
+    sudo('ln -s /data/web_static/releases/web_static_{}/ \
+         /data/web_static/current'.format(timestamp))
